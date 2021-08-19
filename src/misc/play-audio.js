529a6efb4257cb60851c435e229781a9440f07a2
PanchoPC2 <58098908+Panchober27@users.noreply.github.com>
2021-08-19 02:57:07 -0400

    ...

--- src/misc/play-audio.js
+++ src/misc/play-audio.js
@@ -1,26 +1,24 @@
 //  USES COMANDO FRAMEWORK
-const Commando = require('discord.js-commando');
+const Commando = require("discord.js-commando");
 
 module.exports = class PlayAudioComnad extends Commando.Command {
-    constructor(client){
-        super(client, {
-            name: 'playaudio',
-            group: 'misc',
-            memberName: 'playaudio',
-            description: 'Plays some audio'
-        })
+  constructor(client) {
+    super(client, {
+      name: "playaudio",
+      group: "misc",
+      memberName: "playaudio",
+      description: "Plays some audio",
+    });
+  }
+
+  async run(message) {
+    const { voice } = message.member;
+
+    if (!voice.channelID) {
+      message.reply("You must be in a voice channel");
+      return;
     }
 
-    async run(message){
-        const {voice} = message.member
-
-        if(!voice.channelID){
-            message.reply('You must be in a voice channel')
-            return
-        }
-
-        voice.channel.join();
-
-
-    }
-}
+    voice.channel.join();
+  }
+};
